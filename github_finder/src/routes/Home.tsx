837ec5f7e8a4a98aa8ab180189898b023fa81192
CarlosAVDS <carlosalbertovds04@gmail.com>
2026-01-07 11:55:16 -0300

    first commit

---
+++ github_finder/src/routes/Home.tsx
@@ -0,0 +1,47 @@
+import { UserProps } from "../types/user"
+import { useState } from "react"
+import Search from "../components/Search"
+import User from "../components/User"
+import Error from "../components/Error"
+import Loader from "../components/Loader"
+
+const Home = () => {
+    const [user, setUser] = useState<UserProps | null>(null);
+    const [error, setError] = useState<boolean>(false);
+    const [loading, setLoading] = useState<boolean>(false);
+
+    const loadUser = async (userName: string) => {
+        setError(false);
+        setUser(null);
+        setLoading(true);
+        const response = await fetch(`https://api.github.com/users/${userName}`);
+        const data = await response.json();
+
+        setLoading(false);
+        if (response.status === 404) {
+            setError(true);
+            return
+        }
+
+        const { avatar_url, login, location, followers, following } = data;
+        const userData: UserProps = {
+            avatar_url,
+            login,
+            location,
+            followers,
+            following,
+        };
+        setUser(userData);
+    }
+
+    return (
+        <div>
+            <Search loadUser={loadUser} />
+            {loading && <Loader />}
+            {user && <User {...user} />}
+            {error && <Error />}
+        </div>
+    )
+}
+
+export default Home
